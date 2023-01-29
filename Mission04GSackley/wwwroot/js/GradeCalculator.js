184c17834fb5aaf34f4ed406286ec25d067177ad
garrett.sackley@gmail.com <garrett.sackley@gmail.com>
2023-01-28 17:09:46 -0700

    Edits to Grade Calculator

--- Mission04GSackley/wwwroot/js/GradeCalculator.js
+++ Mission04GSackley/wwwroot/js/GradeCalculator.js
@@ -11,7 +11,21 @@ $(document).ready(function () {
         var final = parseFloat($("#final").val());
         var total = (assignments * 0.5) + (quizzes * 0.1) + (projects * 0.1) + (intex * 0.1) + (midterm * 0.1) + (final * 0.1);
         var percentage = total.toFixed(2);
-        $("#grade").text("Your Final Grade is a " + percentage + "%");
 
+        var lettergrade = "";
+
+        if (percentage >= 90) {
+            lettergrade = "A";
+        } else if (percentage >= 80) {
+            lettergrade = "B";
+        } else if (percentage >= 70) {
+            lettergrade = "C";
+        } else if (percentage >= 60) {
+            lettergrade = "D";
+        } else {
+            lettergrade = "F";
+        }
+
+        $("#grade").text("Your Final Grade is a " + lettergrade + ", " + percentage + "%");
     });
 });
